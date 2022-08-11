e6d9c4eda2b335668430abf14fad1d879581f251
speechrezz <thespeechlessmusic@gmail.com>
2022-08-11 16:08:34 -0700

    Testing SendOwl

--- src/routes/plugins.svelte
+++ src/routes/plugins.svelte
@@ -14,6 +14,12 @@
 				quasi. In deleniti eaque aut repudiandae et a id nisi.
 			</p>
 			<button class="btn btn-primary">More info</button>
+			<a href="https://transactions.sendowl.com/products/78746842/4E5367E4/purchase" rel="nofollow">
+				<button class="btn btn-outline">
+					Buy Now
+				</button>
+			</a>
+			<script type="text/javascript" src="https://transactions.sendowl.com/assets/sendowl.js" ></script>
 		</div>
 	</div>
 </div>
